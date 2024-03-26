c94d5ba9879f35be4ccf98169f022b10b89fe9e7
Marci <marcibaumel@gmail.com>
2024-03-26 16:41:24 +0100

    Circle animation complete

---
+++ Spinner.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace loading_spinner_demo
+{
+    public partial class Spinner : UserControl
+    {
+        public Spinner()
+        {
+            InitializeComponent();
+        }
+
+        public Duration Duration
+        {
+            get { return (Duration)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+
+        public Visibility Visibility
+        {
+            get { return (Visibility)GetValue(VisibilityProperty); }
+            set { SetValue(VisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty DurationProperty =
+            DependencyProperty.Register("Duration", typeof(Duration), typeof(Spinner), new PropertyMetadata(default(Duration)));
+
+        public static readonly DependencyProperty VisibilityProperty =
+               DependencyProperty.Register("Visibility", typeof(Visibility), typeof(Spinner), new PropertyMetadata(default(Visibility)));
+
+    }
+}
